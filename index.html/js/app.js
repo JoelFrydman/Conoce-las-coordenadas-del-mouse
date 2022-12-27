16391acf645bd62d5b066dc5fc49331a1b7034a5
Joel Frydman <joelfrydman@hotmail.com>
2022-12-27 17:35:49 -0300

    first commit

---
+++ index.html/js/app.js
@@ -0,0 +1,15 @@
+let IE = document?true:false;
+if (!IE) document(Event.MOUSEMOVE)
+document.onmousemove = getMouseXY;
+let tempX = 0;
+let tempY = 0;
+
+function getMouseXY(e) {
+if (IE) {tempX = event.clientX + document.body.scrollLeft;
+tempY = event.clientY + document.body.scrollTop;}
+else {tempX = e.pageX;tempY = e.pageY;}  
+if (tempX < 0){tempX = 0;}
+if (tempY < 0){tempY = 0;}  
+document.Show.MouseX.value = tempX;
+document.Show.MouseY.value = tempY;
+return true;}
