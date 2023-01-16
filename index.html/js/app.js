cd633e8f8ee631f38ef57abb978763956b5507b8
Joel Frydman <joelfrydman@hotmail.com>
2023-01-16 20:38:01 -0300

    se agrego un prompt y comentarios de orientacion

--- index.html/js/app.js
+++ index.html/js/app.js
@@ -1,9 +1,14 @@
+//prompt de bienvenida e ingreso de datos con salida por consola.
+let username = window.prompt("Bienvenid@ a conoce las coordenadas de tu mouse. Completa tu nombre de usuario");
+console.log('Bienvenido ' + username + ' no olvides pasar por nuestros canales de contacto')
+
 let IE = document?true:false;
 if (!IE) document.addEventListene(Event.MOUSEMOVE)
 document.onmousemove = getMouseXY;
 let tempX = 0;
 let tempY = 0;
 
+//Generacion de funcion con salida por pantalla.
 function getMouseXY(e) {
 if (IE) {tempX = event.clientX + document.body.scrollLeft;
 tempY = event.clientY + document.body.scrollTop;}
@@ -13,4 +18,4 @@ if (tempY < 0){tempY = 0;}
 document.Show.MouseX.value = tempX;
 document.Show.MouseY.value = tempY;
 return true;}
-console.log(getMouseXY)
+
